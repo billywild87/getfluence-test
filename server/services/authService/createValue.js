32199ae77892aed418a9df6bd344634284a51c90
Frédéric Denis <frederic.denis@agenceseize.fr>
2022-06-27 03:05:40 +0200

    modif avatar

--- server/services/authService/createValue.js
+++ server/services/authService/createValue.js
@@ -9,7 +9,7 @@ export async function createUser(id_firebase,email,pseudo){
     console.log(id_firebase,email,pseudo)
     const userQuery={
         text: `INSERT INTO users (id_firebase,email,pseudo,avatar)
-               VALUES ($1,$2,$3,'men_hypster') RETURNING *`,
+               VALUES ($1,$2,$3,'men_hipster') RETURNING *`,
         values: [id_firebase,email,pseudo]
 
     }
